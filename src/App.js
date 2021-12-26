70e5a1bdcf1ffd00f3559a10b63b23365d93aad0
Mahsa Irankhah <mahsairankhah77@gmail.com>
2021-12-26 23:21:30 +0330

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Project</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="http://mahsair.ir" href="_blank">
-          Mahdesign
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/mahdesign/react-weather-app"
-          target="_blank"
-        >
-          open source on github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Project</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="http://mahsair.ir" href="_blank">
+            Mahdesign
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/mahdesign/react-weather-app"
+            target="_blank"
+          >
+            open source on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
